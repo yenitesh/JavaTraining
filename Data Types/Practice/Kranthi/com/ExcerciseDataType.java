dd88903019f547461782e7818fb1e19e58791ade
Arun <srini@srritsolutions.com>
2019-10-31 11:45:23 -0400

    Added members specific folders

---
+++ Data Types/Practice/Kranthi/com/ExcerciseDataType.java
@@ -0,0 +1,22 @@
+package com;
+public class ExcerciseDataType {
+
+	/**
+	 * This data type program you need to create the following:
+	 * 
+	 *  1. Create at least three identifiers
+	 *  2. Create few variables using different data types
+	 *  3. Add few arithmetic operations
+	 *  4. Add few logical operators 	 
+	 *  
+	 */
+
+	public static void main( String[] args ){  
+ 
+		//TODO you need to create your logic here..
+		
+
+	}
+}
+
+
