051408fca93e577bd46012bc44ee2a7b3b31543b
Arun <srini@srritsolutions.com>
2019-10-31 11:41:28 -0400

    Added result programs

---
+++ Java Introduction/Results/com/Hello.java
@@ -0,0 +1,17 @@
+package com;
+
+public class Hello {
+
+	/**
+	 * My first hello world Java program 
+	 * 
+	 **/
+	public static void main( String[] args ){  
+
+		// print the string Hello World! on screen
+		System.out.println("Hello World!");
+
+	}
+}
+
+
