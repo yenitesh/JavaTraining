eec08d6956759027954d521435d3ddf75b0cd004
yenitesh <57229618+yenitesh@users.noreply.github.com>
2019-11-03 08:40:59 -0500

    Update ClassDemo.java

--- src/classes/practice/nitesh/ClassDemo.java
+++ src/classes/practice/nitesh/ClassDemo.java
@@ -1,4 +1,17 @@
 package classes.practice.nitesh;
+package com.oop.gitHub;
+ class A111 {
+	A111(String name, String address){
+System.out.println("My Name is " + name);
+System.out.println("My Address is " + address);
+}
+	
+		A111(int phoneNumber){
+			System.out.println("My Phone Number is " +phoneNumber);
+		}
+ }
+	
+			
 
 public class ClassDemo {
 
@@ -16,9 +29,17 @@ public class ClassDemo {
  
 		//TODO you need to create your logic here..
 		
+		A111 obj1 = new A111("Nitesh Dhungana", "31 Hamlin Rd");
+		A111 obj2 = new A111(800800800);
+		System.out.println("--------------------------");
+		A111 obj3 = new A111("Dhungana  Nitesh", "53 Pisc way");
+		A111 obj4 = new obj4("8118118111);
+		
 		
 
 	}
 }
+
+
 
 
